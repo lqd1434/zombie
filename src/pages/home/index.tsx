a5847bb42c8d6d3e430916d26b5ab9191088ef4e
lqd1434 <1434288209@qq.com>
2021-09-05 22:49:46 +0800

    Initial commit

---
+++ src/pages/home/index.tsx
@@ -0,0 +1,64 @@
+import React from "react";
+import styles from './index.module.scss'
+
+export const Home = () =>{
+	return (
+			<div className={styles.home}>
+				<div className={styles.one}>
+					<div/>
+					<div/>
+					<div/>
+					<div/>
+					<div/>
+					<div/>
+					<div/>
+					<div/>
+					<div/>
+				</div>
+				<div className={styles.two}>
+					<div/>
+					<div/>
+					<div/>
+					<div/>
+					<div/>
+					<div/>
+					<div/>
+					<div/>
+					<div/>
+				</div>
+				<div className={styles.three}>
+					<div/>
+					<div/>
+					<div/>
+					<div/>
+					<div/>
+					<div/>
+					<div/>
+					<div/>
+					<div/>
+				</div>
+				<div className={styles.four}>
+					<div/>
+					<div/>
+					<div/>
+					<div/>
+					<div/>
+					<div/>
+					<div/>
+					<div/>
+					<div/>
+				</div>
+				<div className={styles.five}>
+					<div/>
+					<div/>
+					<div/>
+					<div/>
+					<div/>
+					<div/>
+					<div/>
+					<div/>
+					<div/>
+				</div>
+			</div>
+	)
+}
